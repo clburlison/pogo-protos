b38df06185bd0a55b887fbe9b23aa21d8e060ef5
Furtif <msntchat@hotmail.fr>
2020-12-19 23:17:09 +0100

    195.1 compiled js and ts rpc only 195.1 uses rpc only real names https://github.com/Furtif/POGOProtos/blob/RPC/base/v0.195.1.proto tag: https://github.com/Furtif/POGOProtos/releases/tag/2.53.3

--- build.bat
+++ build.bat
@@ -3,6 +3,6 @@ call npm install -g npm
 call npm update
 call npm audit fix --force
 call npm i
-rem call npm run-script build
-call npm run-script build_js_only
+call npm run-script build
+rem call npm run-script build_js_only
 call npm run-script test
