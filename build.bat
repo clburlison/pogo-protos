11c4f609cefafbc81330ba424d01e6e40b722458
Furtif <msntchat@hotmail.fr>
2021-01-13 21:01:13 +0100

    sync and according version

--- build.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-call npm install -g npm
-call npm update
-call npm audit fix --force
-call npm i
-call npm run-script build
-rem call npm run-script build_js_only
-call npm run-script test
